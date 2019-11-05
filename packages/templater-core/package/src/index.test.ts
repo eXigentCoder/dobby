d6691fe713217af6290794d8723a9b1dbc8e82fb
Matthew van der Velden <matthew.vdvelden@gmail.com>
2019-11-05 12:27:25 +0200

    feat: adds schema validation to templater-core

--- packages/templater-core/package/src/index.test.ts
+++ packages/templater-core/package/src/index.test.ts
@@ -1,19 +1,88 @@
-import { TemplaterCore } from "../src";
+import { TemplaterCore, ValidationError } from "../src";
 
 describe(`templaterCore`, () => {
 	describe(`with a simple template of "hello {{name}}!"`, () => {
 		const template = `Hello {{name}}!`;
 
-		it(`should return 'Hello bob!' `, () => {
-			const templater = new TemplaterCore();
+		describe(`without a schema`, () => {
+			it(`should return 'Hello bob!' `, () => {
+				const templater = new TemplaterCore();
 
-			const model = {
-				name: `bob`,
+				const model = {
+					name: `bob`,
+				};
+
+				const templatedString = templater.bind(template, model);
+
+				expect(templatedString).toEqual(`Hello bob!`);
+			});
+		});
+
+		describe(`with a schema`, () => {
+			const schema = {
+				definitions: {},
+				$schema: `http://json-schema.org/draft-07/schema#`,
+				$id: `http://example.com/root.json`,
+				type: `object`,
+				title: `The Root Schema`,
+				required: [`name`, `age`],
+				properties: {
+					name: {
+						$id: `#/properties/name`,
+						type: `string`,
+						title: `The Name Schema`,
+						default: ``,
+						examples: [`john`],
+						pattern: `^(.*)$`,
+					},
+					age: {
+						$id: `#/properties/age`,
+						type: `integer`,
+						title: `The Age Schema`,
+						default: 0,
+						examples: [30],
+					},
+				},
 			};
 
-			const templatedString = templater.bind(template, model);
+			describe(`with a valid model`, () => {
+				const model = {
+					name: `John`,
+					age: 41,
+				};
 
-			expect(templatedString).toEqual(`Hello bob!`);
+				it(`should return 'Hello John!' `, () => {
+					const templater = new TemplaterCore();
+
+					const templatedString = templater.bind(template, model, schema);
+
+					expect(templatedString).toEqual(`Hello John!`);
+				});
+			});
+
+			describe(`with an invalid model`, () => {
+				const model = {
+					name: `John`,
+					invoiceNumber: `ABC123`,
+				};
+
+				it(`should return 'Hello John!' `, () => {
+					const templater = new TemplaterCore();
+					const expectedError = new ValidationError(`data should have required property 'age'`, [
+						{
+							keyword: `required`,
+							dataPath: ``,
+							schemaPath: `#/required`,
+							params: { missingProperty: `age` },
+							message: `should have required property 'age'`,
+						},
+					]);
+
+					expect(() => {
+						templater.bind(template, model, schema);
+					}).toThrow(expectedError);
+				});
+			});
 		});
 	});
 });
--- packages/templater-core/package/src/index.ts
+++ packages/templater-core/package/src/index.ts
@@ -1,7 +1,27 @@
 import * as handlebars from "handlebars";
+import * as AJV from "ajv";
 
 export class TemplaterCore {
 	bind(template: string, model: object, schema?: object): string {
+		if (schema) {
+			const ajv = new AJV({ allErrors: true });
+			const valid = ajv.validate(schema, model);
+			if (!valid) {
+				const errorMessage = ajv.errorsText();
+				const errorData = ajv.errors || [];
+				throw new ValidationError(errorMessage, errorData);
+			}
+		}
+
 		return handlebars.compile(template)(model);
+	}
+}
+
+export class ValidationError extends Error {
+	data: AJV.ErrorObject[];
+	constructor(message: string, data: AJV.ErrorObject[]) {
+		super(message);
+		this.name = `ValidationError`;
+		this.data = data;
 	}
 }
