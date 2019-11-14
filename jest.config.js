60d0255fbac37e217816ad54463e65de3eceae4a
Ryan Kotzen <potz666@gmail.com>
2019-11-14 08:04:31 +0200

    chore(CI): updating ci process and publishing coverage
    affects: dobby-example-package, dobby-templater-core

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,12 @@ module.exports = {
 	testRegex: `(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$`,
 	moduleFileExtensions: [`ts`, `tsx`, `js`, `jsx`, `json`, `node`],
 	testEnvironment: `node`,
+	coverageThreshold: {
+		global: {
+			statements: 40,
+			branches: 80,
+			functions: 25,
+			lines: 40,
+		},
+	},
 };
